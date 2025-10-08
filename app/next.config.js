c2adec48c537c067074170b6bb9364eebac1ed30
melvn <melvingoxhajl@gmail.com>
2025-10-08 19:13:13 +1100

    configure next.js for static export

--- app/next.config.js
+++ app/next.config.js
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: `${process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000'}/api/:path*`,
-      },
-    ]
+  output: 'export',
+  images: {
+    unoptimized: true,
   },
 }
 
